bf91f2b5136e474c2b1f29a9d9a3632dd17bfde1
Dotun Longe <funnica4@gmail.com>
2022-12-19 05:14:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ mongoose = require("mongoose");
 books = require("./models/bookModel");
 
 mongoose.Promise = global.Promise;
+
+// local db connection, url can be changed to seperate production url
 mongoose.connect("mongodb://localhost:27017/BooksDB");
 
 routes = require("./routes/bookRoutes");
@@ -14,5 +16,5 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 routes(app);
-
+// start server 
 app.listen(port);
